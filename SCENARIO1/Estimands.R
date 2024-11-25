85de1ee9685d9e2c10b06d0bc038d79d35a326c6
Veronica Ballerini <115488016+veronicaballerini@users.noreply.github.com>
2024-11-25 18:25:55 +0100

    Update Estimands.R
    estimand weights

--- SCENARIO1/Estimands.R
+++ SCENARIO1/Estimands.R
@@ -6,26 +6,10 @@ ce <- function(theta, d,complete.data, scenario){
   ID<-complete.data$ID
   D<-complete.data$D
 
-  m1_d<-mean(x1[ID==0])
-  m2_d<-mean(x2[ID==0])
-  m3_d<-mean(x3[ID==0])
-  
-  m1_nd<-mean(x1[ID==1])
-  m2_nd<-mean(x2[ID==1])
-  m3_nd<-mean(x3[ID==1])
-  
-  m1_ed<-mean(x1[ID==0&D<median(D[ID==0])])
-  m2_ed<-mean(x2[ID==0&D<median(D[ID==0])])
-  m3_ed<-mean(x3[ID==0&D<median(D[ID==0])])
-  
-  m1_ld<-mean(x1[ID==0&D>=median(D[ID==0])])
-  m2_ld<-mean(x2[ID==0&D>=median(D[ID==0])])
-  m3_ld<-mean(x3[ID==0&D>=median(D[ID==0])])
-  
   EY1<-NULL
   EY0<-NULL
   
-  for(i in which(ID==1)){
+  for(i in nrow(complete.data)){
     EY1 <- sum(EY1,exp(-(theta$beta.y1nd+
                            theta$eta.y1*x1[i]+
                            theta$eta.y2*x2[i]+
@@ -36,7 +20,7 @@ ce <- function(theta, d,complete.data, scenario){
                            theta$eta.y3*x3[i])/theta$alpha.y0nd)*gamma(1+{1/theta$alpha.y0nd})*theta$pi[i])
   }
   
-  pG<-sum(theta$pi[ID==1])
+  pG<-sum(theta$pi)
   
   EY1.nd <- EY1/pG
   EY0.nd <- EY0/pG
@@ -47,16 +31,21 @@ ce <- function(theta, d,complete.data, scenario){
   rm(EY1,EY0,pG)
   
   ace.nd<- EY1.nd-EY0.nd
-  
-  pG<-sum(1-theta$pi[ID==0])
-  
-  ace.d <- ace.fd <- EY0.d_save <- EY0.d_save1 <- EY0.d_save2 <- NULL
+    
+  ace.d <- ace.fd <- NULL
   
   for(h in 1:length(d)){
     
-    EY0 <- EY1 <- NULL
+    EY0 <- EY1 <- ph <- NULL
     
-    for(i in which(ID==0)){
+    for(i in nrow(complete.data)){
+      ph <- rbind(ph, dweib(d[h],
+                            a=theta$alpha.d,
+                            b=theta$beta.d+
+                              theta$eta.y1*x1[i]+
+                              theta$eta.y2*x2[i]+
+                              theta$eta.y3*x5[i]))
+      
       EY1 <- sum(EY1,Weib.Trunc.Moments(order=1,
                                         shape=theta$alpha.y1d,
                                         scale=exp(-(theta$beta.y1d+
@@ -64,13 +53,14 @@ ce <- function(theta, d,complete.data, scenario){
                                                       theta$eta.y2*x2[i]+
                                                       theta$eta.y3*x3[i]+
                                                       theta$lambda*log(d[h]))/theta$alpha.y1d),
-                                        left=d[h])*(1-theta$pi[i]))
+                                        left=d[h]))
+      
       if(scenario==1){
         EY0 <- sum(EY0,(exp(-(theta$beta.y0d+
                                 theta$lambda*log(d[h])+
                                 theta$eta.y1*x1[i]+
                                 theta$eta.y2*x2[i]+
-                                theta$eta.y3*x3[i])/theta$alpha.y0d)*gamma(1+{1/theta$alpha.y0d}))*(1-theta$pi[i]))
+                                theta$eta.y3*x3[i])/theta$alpha.y0d)*gamma(1+{1/theta$alpha.y0d})))
       }else{
         EY0 <- sum(EY0,Weib.Trunc.Moments(order=1,
                                           shape=theta$alpha.y0d,
@@ -79,44 +69,17 @@ ce <- function(theta, d,complete.data, scenario){
                                                         theta$eta.y2*x2[i]+
                                                         theta$eta.y3*x3[i]+
                                                         theta$lambda*log(d[h]))/theta$alpha.y0d),
-                                          left=d[h])*(1-theta$pi[i]))
+                                          left=d[h]))
       }
 
     }
+
+    pG<-sum(ph)
+        
+    ace.d[h] <- sum((EY1-EY0)*(ph/pG))
     
-    EY1.d <- EY1/pG
-    EY0.d <- EY0/pG
-    
-    EY0.d_save[h] <- EY0.d
-    EY0.d_save1[h] <- EY0.d*dweib(h,
-                                 a=theta$alpha.d,
-                                 b=theta$beta.d+
-                                   theta$eta.y1*x1[i]+
-                                   theta$eta.y2*x2[i]+
-                                   theta$eta.y3*x3[i])
-    EY0.d_save2[h] <- EY0*dweib(h,
-                                a=theta$alpha.d,
-                                b=theta$beta.d+
-                                  theta$eta.y1*x1[i]+
-                                  theta$eta.y2*x2[i]+
-                                  theta$eta.y3*x3[i])
-    
-    rm(EY0,EY1)
-    
-    ace.d[h] <- EY1.d-EY0.d
-    
-    ace.fd[h] <- ace.d[h]*dweib(h,
-                                a=theta$alpha.d,
-                                b=theta$beta.d+
-                                  theta$eta.y1*x1[i]+
-                                  theta$eta.y2*x2[i]+
-                                  theta$eta.y3*x3[i])
-    
-    rm(EY1.d,EY0.d)
-  }
-  
-  EY0.d_overall <- sum(EY0.d_save1)
-  EY0_overall <- (EY0.nd_save2+sum(EY0.d_save2))/(sum(theta$pi[ID==1])+sum(1-theta$pi[ID==0]))
+    ace.fd[h] <- ace.d[h]*pG*(1/nrow(complete.data))
+}
   
   pND<-sum(ID==1)/nrow(complete.data)
   pD<-1-pND
@@ -124,12 +87,7 @@ ce <- function(theta, d,complete.data, scenario){
   aceoverall<-ace.nd*pND + sum(ace.fd)*pD
   
   list(ace.nd=ace.nd, ace.d=ace.d, ace.fd=ace.fd,
-       aceoverall=aceoverall,EY0.nd=EY0.nd_save,EY0.d=EY0.d_save,
-       EY0.d_overall=EY0.d_overall,EY0_overall=EY0_overall,
-       m1_d=m1_d, m2_d=m2_d, m3_d=m3_d, 
-       m1_nd=m1_nd, m2_nd=m2_nd, m3_nd=m3_nd, 
-       m1_ed=m1_ed, m2_ed=m2_ed, m3_ed=m3_ed,
-       m1_ld=m1_ld, m2_ld=m2_ld, m3_ld=m3_ld)
+       aceoverall=aceoverall)
 }
 
 dce <- function(theta, d, y, complete.data){
